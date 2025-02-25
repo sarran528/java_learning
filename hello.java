c9b8b85fa9e5454a75d68242a087e582dde7f2e7
sarran528 <sarranm528@gmail.com>
2025-02-25 23:21:47 +0530

    Added Java file

---
+++ hello.java
@@ -0,0 +1,32 @@
+// class hello
+// {
+
+//  public static void main( String a[])
+//     {
+//         byte u = 34;
+//         int r= 48;
+//     System.out.println(u-r);
+//     }
+// }
+// float and double
+// class hello
+// {
+
+//  public static void main( String a[])
+//     {
+//         double p=22.0;
+//         float u=7.0f;
+//     System.out.println(p/u);
+//     }
+// }
+
+class hello
+{
+
+ public static void main( String a[])
+    {
+       int p = 3;
+
+    System.out.println(3<2);
+    }
+}git remote add origin <repository-url>
